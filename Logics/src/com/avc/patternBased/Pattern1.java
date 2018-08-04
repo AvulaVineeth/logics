054e45d2306f5642e4666d7ab255a3f1a78dc19a
AvulaVineeth <avulavineethchowdary@gmail.com>
2018-08-04 13:33:50 +0530

    Java Modules Logics

--- Logics/src/com/avc/patternBased/Pattern1.java
+++ Logics/src/com/avc/patternBased/Pattern1.java
@@ -4,10 +4,22 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(),width,length,i;
+		int n = sc.nextInt(),i,j,h,noOfColumns=0,k=n+3;
 		String[] a= {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-		for(width=n,length=1,i=0;i<n;width+=2,length++,i++) {
-			System.out.printf("%*.*s",width,length,a[(i*i)]);
+		for(i=0;i<n;i++) {
+			h=i*i;
+			for(int f=0;f<k;f++)
+			{
+				System.out.print(" ");
+			}
+		for(j=0;j<=noOfColumns;j++) {
+			System.out.print(a[h]+" ");
+			h+=1;
+		}
+		System.out.println();
+		noOfColumns+=2;
+		k-=2;
+		
 		}
 		sc.close();
 	}
---
+++ Logics/src/com/avc/patternBased/Pattern3.java
@@ -0,0 +1,5 @@
+package com.avc.patternBased;
+
+public class Pattern3 {
+
+}
